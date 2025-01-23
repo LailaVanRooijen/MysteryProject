27e1650a9195e81331fe86537de65d935351a874
Laila <175353500+LailaVanRooijen@users.noreply.github.com>
2025-01-23 16:29:51 +0100

    OAuth config
    feat(auth): add basic OAuth configuration
    
    Added initial configuration for OAuth to support authentication functionality.

--- backend/src/main/java/com/mystery/project/entities/hello/HelloController.java
+++ backend/src/main/java/com/mystery/project/entities/hello/HelloController.java
@@ -1,18 +1,20 @@
 package com.mystery.project.entities.hello;
 
 import com.mystery.project.mainconfiguration.Routes;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.*;
 
 // Temporary hello endpoint as per request
 
 @RestController
 @RequestMapping(Routes.BASE_ROUTE)
 public class HelloController {
-    @GetMapping
-    public String sayHello(){
-        return "Well hello there!";
+  @GetMapping
+  public String sayHello(@AuthenticationPrincipal OAuth2User principal) {
+    if (principal != null) {
+      return "Hello " + principal.getAttribute("name");
     }
+    return "Not authenticated";
+  }
 }
