d6d43bab7d2276043f7e26bf26304ad9e90afa4a
Ivan Banha <banha.ivan@gmail.com>
2025-01-25 17:29:29 +0200

    chore: Bump up tailwind version. Add DaisyUI

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,4 +1,5 @@
 export const plugins = {
+  plugins: ["@tailwindcss/postcss"],
   tailwindcss: {},
   autoprefixer: {},
 };
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
-import "@app/App.css";
-import "@app/index.css";
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="flex h-full flex-col items-center justify-center gap-16">
+      <h1 className="text-6xl font-bold underline">Hello world!</h1>
+      <button className="btn btn-lg btn-primary">
+        I'm a button. Click me!
+      </button>
+    </div>
   );
 }
 
