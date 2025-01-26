a1c47f30cf434f61c655fc059064fda5f7c123db
Ivan Banha <banha.ivan@gmail.com>
2025-01-26 15:56:11 +0200

    feat: Add axios

---
+++ frontend/src/api/client.ts
@@ -0,0 +1,55 @@
+import { Axios, AxiosRequestConfig } from "axios";
+
+const baseURL = new URL("/api/v1", import.meta.env.VITE_BACKEND_URL);
+
+class ApiClient extends Axios {
+  #interceptorIDs: Record<string, number> = {};
+  #refreshToken?: string | null;
+
+  constructor(config?: AxiosRequestConfig) {
+    super(config);
+
+    this.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response && error.response.status === 401) {
+          // Implement logic to refresh access token here
+          // Handle token invalidation, e.g., redirect to login
+          console.error("Unauthorized access - possibly invalid token");
+        }
+
+        return Promise.reject(error);
+      },
+    );
+  }
+
+  setTokens(accessToken: string, refreshToken: string) {
+    this.#refreshToken = refreshToken;
+
+    this.#interceptorIDs["auth"] = this.interceptors.request.use(
+      (cfg) => {
+        cfg.headers.Authorization = `Bearer ${accessToken}`;
+
+        return cfg;
+      },
+      (cfg) => cfg,
+    );
+  }
+
+  clearTokens() {
+    this.#refreshToken = null;
+
+    const interceptorId = this.#interceptorIDs["auth"];
+
+    if (interceptorId !== undefined) {
+      this.interceptors.request.eject(interceptorId);
+      delete this.#interceptorIDs["auth"];
+    }
+  }
+}
+
+const apiClient = new ApiClient({
+  baseURL: baseURL.toString(),
+});
+
+export default apiClient;
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,21 @@
 import { Link } from "react-router";
 
+import apiClient from "@app/api/client";
+import { sayHello } from "@app/api/core";
+
 const Home = () => {
   return (
     <div className="flex h-full flex-col items-center justify-center gap-16">
       <h1 className="text-6xl font-bold underline">Home Page!</h1>
-      <button className="btn btn-lg btn-primary">
+      <button
+        className="btn btn-lg btn-primary"
+        onClick={async () => {
+          // Demo
+          await sayHello();
+          apiClient.setTokens("access_token_value", "refresh_token_value");
+          await sayHello();
+        }}
+      >
         I'm a button. Click me!
       </button>
       <Link to="about" className="link link-info">
