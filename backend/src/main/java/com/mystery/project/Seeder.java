ed01b4e3081baebbaf1bc1f7d2a89e87303a9baf
Laila <175353500+LailaVanRooijen@users.noreply.github.com>
2025-01-31 18:07:44 +0100

    feat(swagger) chore(seeder)
    feat(swagger): add Swagger dependency for auto-generated API documentation.
    
    Added Swagger dependency to enable automatic endpoint documentation.
    API documentation can be accessed at /swagger-ui/index.html
    
    chore(seeder): update seeder to include two users

--- backend/src/main/java/com/mystery/project/Seeder.java
+++ backend/src/main/java/com/mystery/project/Seeder.java
@@ -1,14 +1,25 @@
 package com.mystery.project;
 
-import lombok.AllArgsConstructor;
+import com.mystery.project.authentication.AuthenticationService;
+import com.mystery.project.authentication.dtos.RegisterRequestDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class Seeder implements CommandLineRunner {
-    @Override
-    public void run(String... args) throws Exception {
-        // When creating methods to seed database with mock data, you can call them here
-    }
+  private final AuthenticationService authenticationService;
+
+  @Override
+  public void run(String... args) throws Exception {
+    seedUsers();
+  }
+
+  private void seedUsers() {
+    authenticationService.registerUser(
+        new RegisterRequestDto("chad@gmail.com", "Password123!", "chad"));
+    authenticationService.registerUser(
+        new RegisterRequestDto("chadette@gmail.com", "Password123!", "chadette"));
+  }
 }
