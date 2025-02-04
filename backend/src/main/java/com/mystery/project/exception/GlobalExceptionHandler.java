02688513ed5098e2a18af5af2daba0e0f104e67b
Laila <175353500+LailaVanRooijen@users.noreply.github.com>
2025-02-04 18:05:32 +0100

    feat(validation dependencies)
    Added dependencies for validation
    
    https://www.baeldung.com/spring-boot-bean-validation

--- backend/src/main/java/com/mystery/project/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/mystery/project/exception/GlobalExceptionHandler.java
@@ -1,17 +1,31 @@
 package com.mystery.project.exception;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<ProblemDetail> badRequestHandler(BadRequestException exception) {
-        ProblemDetail problemDetail =
-                ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
-        return ResponseEntity.badRequest().body(problemDetail);
-    }
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(
+      MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult()
+        .getFieldErrors()
+        .forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+
+    return ResponseEntity.badRequest().body(errors);
+  }
+
+  @ExceptionHandler(BadRequestException.class)
+  public ResponseEntity<ProblemDetail> badRequestHandler(BadRequestException exception) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, exception.getMessage());
+    return ResponseEntity.badRequest().body(problemDetail);
+  }
 }
